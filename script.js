e0298a8e6356aadef5f280be694464cb1e82a695
Nika Maisuradze <103008084+Flexyy19@users.noreply.github.com>
2025-02-16 23:20:31 +0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+// const burgerMenu = document.querySelector(".burger-menu");
+// const navLinks = document.querySelector(".nav-links");
+// const navBar = document.querySelector(".nav-bar");
+
+// burgerMenu.addEventListener("click", function () {
+//   navLinks.classList.toggle("active");
+//   navBar.classList.toggle("active");
+// });
