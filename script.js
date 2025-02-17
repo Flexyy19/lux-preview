0290820b946b9d25b95437600330d422b50e7950
Nika Maisuradze <103008084+Flexyy19@users.noreply.github.com>
2025-02-17 22:00:59 +0400

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-// const burgerMenu = document.querySelector(".burger-menu");
-// const navLinks = document.querySelector(".nav-links");
-// const navBar = document.querySelector(".nav-bar");
-
-// burgerMenu.addEventListener("click", function () {
-//   navLinks.classList.toggle("active");
-//   navBar.classList.toggle("active");
-// });
